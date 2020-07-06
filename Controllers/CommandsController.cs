ac735e81a60274ab25b9261ad2f590d583bcc9c3
Joseph George <jgeorge@n-focus.com>
2020-07-06 16:38:12 -0400

    55:00 mark on the tutorial

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -5,29 +5,35 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Commander.Controllers
 {
-    // //api/commands (this is a 'wild card' approach)
-    // [Route("api/[controller]")]
-    [Route("api/commands")]
-    [ApiController]
-    public class CommandsController: ControllerBase
-    {
-        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
-        //GET api/commands
-        [HttpGet]
-        public ActionResult <IEnumerable<Command>> GetAllCommands()
-        {
-            //throw new System.NotImplementedException();
-            var commandItems = _repository.GetAppCommands();
-            return Ok(commandItems);
-        }
+  // //api/commands (this is a 'wild card' approach)
+  // [Route("api/[controller]")]
+  [Route("api/commands")]
+  [ApiController]
+  public class CommandsController : ControllerBase
+  {
+    private readonly ICommanderRepo _repository;
 
-        //GET api/commands/5
-        [HttpGet("{id}")]
-        public ActionResult <Command> GetCommandById(int id)
-        {
-            //throw new System.NotImplementedException();
-            var commandItem = _repository.GetCommandById(id);
-            return Ok(commandItem);
-        }
+    public CommandsController(ICommanderRepo repository)
+    {
+      _repository = repository;
     }
+    //private readonly ICommanderRepo _repository = new MockCommanderRepo();
+    //GET api/commands
+    [HttpGet]
+    public ActionResult<IEnumerable<Command>> GetAllCommands()
+    {
+      //throw new System.NotImplementedException();
+      var commandItems = _repository.GetAppCommands();
+      return Ok(commandItems);
+    }
+
+    //GET api/commands/5
+    [HttpGet("{id}")]
+    public ActionResult<Command> GetCommandById(int id)
+    {
+      //throw new System.NotImplementedException();
+      var commandItem = _repository.GetCommandById(id);
+      return Ok(commandItem);
+    }
+  }
 }
--- Data/MockCommanderRepo.cs
+++ Data/MockCommanderRepo.cs
@@ -3,24 +3,24 @@ using Commander.Models;
 
 namespace Commander.Data
 {
-    public class MockCommanderRepo : ICommanderRepo
+  public class MockCommanderRepo : ICommanderRepo
+  {
+    public IEnumerable<Command> GetAppCommands()
     {
-        public IEnumerable<Command> GetAppCommands()
-        {
-            //throw new System.NotImplementedException();
-            var commands = new List<Command>
+      //throw new System.NotImplementedException();
+      var commands = new List<Command>
             {
                 new Command{Id=0, HowTo="Boil an egg", Line="Boil water", Platform="Kettle & Pan"},
                 new Command{Id=1, HowTo="Slice Bread", Line="Get a knife", Platform="Cutting board"},
                 new Command{Id=2, HowTo="Cup o' Tea", Line="Tea bag in cup", Platform="Kettle & Cup"},
             };
-            return commands;
-        }
-
-        public Command GetCommandById(int Id)
-        {
-            //throw new System.NotImplementedException();
-            return new Command{Id=0, HowTo="Boil an egg", Line="Boil water", Platform="Kettle & Pan"};
-        }
+      return commands;
     }
+
+    public Command GetCommandById(int Id)
+    {
+      //throw new System.NotImplementedException();
+      return new Command { Id = Id, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle & Pan" };
+    }
+  }
 }
